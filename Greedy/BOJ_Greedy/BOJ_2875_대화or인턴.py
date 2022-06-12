43049ad4d58c200cd4fac324dea8d444d74900aa
seoulmango <gpark0503@gmail.com>
2022-06-12 21:34:23 +0900

    BOQ 대회or인턴 added

---
+++ Greedy/BOJ_Greedy/BOJ_2875_대화or인턴.py
@@ -0,0 +1,32 @@
+import sys
+input = sys.stdin.readline
+
+w, m, k = map(int, input().split())
+
+count = 0
+
+while True:
+    w -= 2
+    m -= 1
+    count += 1
+
+    if w < 0 or m < 0:
+        count -= 1
+        w += 2
+        m += 1
+        break
+
+    elif w == 0 or m == 0:
+        break
+
+if w+m >= k:
+    print(count)
+else:
+    while True:
+        count -= 
+        k -= 3
+        if w+m >= k:
+            break
+        elif count == 0:
+            break
+    print(count)
